0d3037b11b925d2f37eb9800522ad5c33fe4a7fb
franksh <frnkschlssr@gmail.com>
2022-03-16 17:39:59 +0100

    change model to 400M

--- chatbot/language_model.py
+++ chatbot/language_model.py
@@ -8,8 +8,8 @@ class LanguageModel():
     """
     def __init__(self):
 
-        # model_name = "facebook/blenderbot-400M-distill"
-        model_name = "facebook/blenderbot-3B"
+        model_name = "facebook/blenderbot-400M-distill"
+        # model_name = "facebook/blenderbot-3B"
 
         self.tokenizer = BlenderbotTokenizer.from_pretrained(model_name)
         self.model = BlenderbotForConditionalGeneration.from_pretrained(model_name)
