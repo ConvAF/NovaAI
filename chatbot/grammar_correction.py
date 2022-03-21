ea90dd55821468584611558610351a4a803a2414
aloisvg <alois.villa@gmail.com>
2022-03-21 16:39:05 +0100

    remove set last user input and add it as parameter in other functions instead

--- chatbot/grammar_correction.py
+++ chatbot/grammar_correction.py
@@ -9,21 +9,16 @@ class GrammarModel(Gramformer):
     """
     def __init__(self, models=1, use_gpu=False, seed=1212):
         self.gm = super().__init__(models=1, use_gpu=False)
-        self.last_user_input = None
 
 
-    def set_last_user_input(self, chat_history):
-        self.last_user_input = chat_history[-1].get('text')
-
-
-    def grammar_correction(self):
+    def grammar_correction(self,last_user_input):
         """
         Generate a corrected sentence and a message to the user with the correction.
         """
-        corrected_sentence = self.correct(self.last_user_input, max_candidates=1)
+        corrected_sentence = self.correct(last_user_input, max_candidates=1)
         corrected_sentence = list(corrected_sentence)[0]
 
-        if corrected_sentence != self.last_user_input:
+        if corrected_sentence != last_user_input:
             correction_message = f"[Correction] {corrected_sentence}"
             print(correction_message)
 
@@ -35,8 +30,9 @@ class GrammarModel(Gramformer):
         Append the message to the user to the chat history.
         Return the corrected sentence.
         """
-        corrected_sentence, correction_message = self.grammar_correction()
-        error_types = self.get_edits(corrected_sentence)
+        last_user_input = chat_history[-1].get('text')
+        corrected_sentence, correction_message = self.grammar_correction(last_user_input)
+        error_types = self.get_edits(last_user_input, corrected_sentence)
 
         if correction_message:
             chat_history.append(
@@ -48,13 +44,13 @@ class GrammarModel(Gramformer):
         return chat_history       
 
 
-    def _get_edits(self, corrected_sentence):
+    def _get_edits(self, input_sentence, corrected_sentence):
         """
         Return the type of the error.
         """
-        self.last_user_input = self.annotator.parse(self.last_user_input)
+        input_sentence = self.annotator.parse(input_sentence)
         corrected_sentence = self.annotator.parse(corrected_sentence)
-        alignment = self.annotator.align(self.last_user_input, corrected_sentence)
+        alignment = self.annotator.align(input_sentence, corrected_sentence)
         edits = self.annotator.merge(alignment)
 
         if len(edits) == 0:  
