df8740047760dc6d83979a916e7277e67074cf3b
aloisvg <alois.villa@gmail.com>
2022-03-22 17:27:44 +0100

    add test for grammar correction model

---
+++ tests/test_grammar_correction.py
@@ -0,0 +1,43 @@
+import pytest
+from flask import session
+from chatbot.db import get_db
+from chatbot.grammar_correction import GrammarModel
+
+
+def test_grammar_correction_model_response(app):
+    """ Test the responses of the grammar correction model """
+    # Load language model (not loaded in test app by default)
+    app.grammar_correction = GrammarModel(models = 1, use_gpu=False)
+
+    # Language model is loaded
+    assert app.grammar_correction
+
+    # test Variables
+    test_input_sentence = "Thanks you."
+    test_corrected_sentence = "Thank you."
+
+    # Grammar correction
+    corrected_sentence, correction_message = app.grammar_correction.grammar_correction(test_input_sentence)
+    assert corrected_sentence != test_input_sentence
+    assert correction_message != None
+    assert test_corrected_sentence in correction_message
+
+    # A valid response is created for each history
+    # 1. The sentence is correct (no correction).
+    chat_history_correct = [{'sender': 'User', 'text': 'Thank you.'}]
+    chat_history_correct_old = chat_history_correct.copy()
+    chat_history_response_correct = app.grammar_correction.add_correction_to_chat_history(chat_history_correct)
+    # The chat history has not increased (no correction)
+    assert len(chat_history_response_correct) == len(chat_history_correct_old)
+
+    # 2. The sentence is incorrect (correction).
+    chat_history_incorrect = [{'sender': 'User', 'text': 'Thanks you.'}]
+    chat_history_incorrect_old = chat_history_incorrect.copy()
+    chat_history_response_incorrect = app.grammar_correction.add_correction_to_chat_history(chat_history_incorrect)
+    # The chat history has increased by 1 (correction)
+    assert len(chat_history_response_incorrect) == len(chat_history_incorrect_old) + 1
+
+    # In case of error, an error type is returned.s
+
+    error_type = app.grammar_correction._get_edits(test_input_sentence, test_corrected_sentence)
+    assert len(error_type) != 0
