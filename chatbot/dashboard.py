dbe24a837b3a9ad8a1767fb01fd5c149454ee11f
franksh <frnkschlssr@gmail.com>
2022-03-25 12:05:27 +0100

    add statistics overview/learning center

--- chatbot/dashboard.py
+++ chatbot/dashboard.py
@@ -1,15 +1,10 @@
 """
 Dashboard view.
 """
-from flask import Blueprint, render_template, request, session, current_app
-from wtforms import Form, StringField, validators
-import plotly.express as px
-import pandas as pd
-import numpy as np
-from datetime import datetime
-from plotly.io import to_html
+from flask import Blueprint, render_template
 
 from chatbot.auth import login_required
+from chatbot.utils import get_activity_plot, get_error_distribution_plot
 
 # Create a blueprint for authentication
 bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
@@ -20,35 +15,24 @@ def dashboard():
     """ Route for the user dashboard
     """
 
-    fig_html = get_plot()
-    
+    fig_activity_html = get_activity_plot()
         
-    return render_template('dashboard/dashboard.html', plot=fig_html)
+    return render_template('dashboard/dashboard.html', plot=fig_activity_html)
 
 
-def get_plot():
-    from datetime import datetime, timedelta
 
-    def get_daily_chat_data():
-        today = datetime.today()
-        date_range = pd.date_range(today-timedelta(days=14), today)
-        chats = [0,1,5,4,0,3,2,3,0,2,0,1,3,2,4]
-        data = pd.DataFrame({'date': date_range, 'chats': chats})
-        return data
+@bp.route('/statistics', methods=('GET',))
+@login_required
+def statistics():
+    """ Route for the user learning center
+    """
 
+    fig_activity_html = get_activity_plot()
 
-    # data_canada = px.data.gapminder().query("country == 'Canada'")
-    data = get_daily_chat_data()
-    fig = px.bar(data, x='date', y='chats', height=300,
-                labels={'chats': 'chats', 'date': ''}
-                )
-    fig.data[0].marker.color = '#17C3B2'
-    # fig = fig.update_layout(displayModeBar= False)
-    fig.update_traces(
-        hoverinfo='none',
-        hovertemplate=None
-    )
-    # fig.update_xaxes({'visible': False})
+    fig_grammar_html = get_error_distribution_plot()
+        
+    return render_template('dashboard/statistics.html', 
+                            plot_activity=fig_activity_html,
+                            plot_grammar=fig_grammar_html,
+                            )
 
-    fig_html = to_html(fig, full_html=False, include_plotlyjs=False, config={"displayModeBar": False})
-    return fig_html
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -17,3 +17,19 @@ def test_dashboard_view(client, auth):
 
         response = client.get('/dashboard/')
         assert response.status_code == 200
+
+def test_dashboard_statitstics_view(client, auth):
+    """ Test whether the view works """
+    # Test GET
+    # Not logged in request redirects to login
+    response = client.get('/dashboard/statistics')
+    assert response.status_code == 302
+    assert 'http://localhost/auth/login' == response.headers['Location']
+
+    # If logged in, view is returned
+    with client: # Inside app context
+        # Logged in returns template
+        auth.login()    
+
+        response = client.get('/dashboard/statistics')
+        assert response.status_code == 200
